a1f8de23c086d0da84738d1d69628565946eb8ea
Kris <kr.jakubonis@gmail.com>
2022-09-19 13:28:37 +0100

    Fixed key extraction by adding access tokens

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ def getKeys(fileName):
     all_keys = open(fileName, 'r').read().splitlines()
     api_key = all_keys[0]
     api_key_secret = all_keys[1]
-    bearer_token = all_keys[2]
-    return api_key, api_key_secret, bearer_token
+    access_token = all_keys[2]
+    access_secret = all_keys[3]
+    return api_key, api_key_secret, access_token, access_secret
 
-AK, AKS, BT = getKeys(file)
+AK, AKS, AT, AS = getKeys(file)
+
+authenticator = tweepy.OAuthHandler(AK, AKS)
