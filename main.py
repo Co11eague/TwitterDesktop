d7680c98fe2838264c93dcd800e1e9f54861dba0
Kris <kr.jakubonis@gmail.com>
2022-09-20 14:46:26 +0100

    Ehhh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tweepy
 import tkinter as tk
 
+global AK, AKS, AT, AS
 
 file = "TwitterKeys"
 def getKeys(fileName):
@@ -10,43 +11,56 @@ def getKeys(fileName):
     access_token = all_keys[2]
     access_secret = all_keys[3]
     return api_key, api_key_secret, access_token, access_secret
+def getKeysEntry():
+    pass
 
-AK, AKS, AT, AS = getKeys(file)
+class Frame1(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+
+class LogInWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Twitter Desktop")
+        self.geometry(str(int(self.winfo_screenwidth()*0.7)) + "x" + str(int(self.winfo_screenheight()*0.7)))
+        self.__create_top_widgets()
+        self.__create_bottom_widgets()
+    def __create_top_widgets(self):
+        self.logIn = tk.Label(self, text = "Log in", font = ("Arial", 80, 'bold'))
+        self.logIn.pack(pady = 100)
+        self.description = tk.Label(self, text = "Enter your information to access your Twitter account", font = ("Arial", 20, 'bold'))
+        self.description.pack(pady=30)
+        self.window = Frame1(self)
+        self.window.pack()
+    def __create_bottom_widgets(self):
+        self.api = tk.Label(self, text = "API Key:", font = ("Arial", 20, 'bold'), anchor="w").grid(row = 2, column = 0, sticky = "e")
+        self.apis = tk.Label(self, text = "API Secret Key:", font = ("Arial", 20, 'bold')).grid(row = 3, column = 0, sticky = "e")
+        self.access = tk.Label(self, text = "Access Token:", font = ("Arial", 20, 'bold')).grid(row = 4, column = 0, sticky = "e")
+        self.accesss = tk.Label(self, text = "Access Secret Token:", font = ("Arial", 20, 'bold')).grid(row = 5, column = 0, sticky = "e")
+
+        self.api_entry = tk.Entry(self, width = int(self.winfo_screenwidth()*0.025))
+        self.api_entry.grid(row = 2, column = 1, sticky = "w")
+        self.api_secret_entry = tk.Entry(self, width = int(self.winfo_screenwidth()*0.025))
+        self.api_secret_entry.grid(row = 3, column = 1, sticky = "w")
+        self.access_token = tk.Entry(self, width = int(self.winfo_screenwidth()*0.025))
+        self.access_token.grid(row = 4, column = 1, sticky = "w")
+        self.access_token_secret = tk.Entry(self, width = int(self.winfo_screenwidth()*0.025))
+        self.access_token_secret.grid(row = 5, column = 1, sticky = "w")
+        self.window = Frame1(self)
+        self.window.pack()
+        self.tk.Button(self, text = "Submit", width = 70, command = getKeysEntry).pack(pady = 40)
+
+if __name__=="__main__":
+    app = LogInWindow()
+    app.mainloop()
+
+AK, AKS, AT, AS = getKeysEntry(file)
 
 authenticator = tweepy.OAuthHandler(AK, AKS)
 authenticator.set_access_token(AT,AS)
 
 api = tweepy.API(authenticator, wait_on_rate_limit=True)
 
-window = tk.Tk()
-top_frame = tk.Frame(window)
-top_frame.pack()
 
-bottom_frame = tk.Frame(window)
-bottom_frame.pack()
-
-window.title("Twitter Desktop")
-window.geometry(str(int(window.winfo_screenwidth()*0.7)) + "x" + str(int(window.winfo_screenheight()*0.7)))
-
-tk.Label(top_frame, text = "Log in", font = ("Arial", 80, 'bold')).pack(pady = 100)
-tk.Label(top_frame, text = "Enter your information to access your Twitter account", font = ("Arial", 20, 'bold')).pack(pady = 30)
-
-tk.Label(bottom_frame, text = "API Key:", font = ("Arial", 20, 'bold'), anchor="w").grid(row = 2, column = 0, sticky = "e")
-tk.Label(bottom_frame, text = "API Secret Key:", font = ("Arial", 20, 'bold')).grid(row = 3, column = 0, sticky = "e")
-tk.Label(bottom_frame, text = "Access Token:", font = ("Arial", 20, 'bold')).grid(row = 4, column = 0, sticky = "e")
-tk.Label(bottom_frame, text = "Access Secret Token:", font = ("Arial", 20, 'bold')).grid(row = 5, column = 0, sticky = "e")
-
-api_entry = tk.Entry(bottom_frame, width = int(window.winfo_screenwidth()*0.025))
-api_entry.grid(row = 2, column = 1, sticky = "w")
-api_secret_entry = tk.Entry(bottom_frame, width = int(window.winfo_screenwidth()*0.025))
-api_secret_entry.grid(row = 3, column = 1, sticky = "w")
-access_token = tk.Entry(bottom_frame, width = int(window.winfo_screenwidth()*0.025))
-access_token.grid(row = 4, column = 1, sticky = "w")
-access_token_secret = tk.Entry(bottom_frame, width = int(window.winfo_screenwidth()*0.025))
-access_token_secret.grid(row = 5, column = 1, sticky = "w")
-
-tk.Button(window, text = "Submit", width = 70).pack(pady = 40)
-
-
-window.mainloop()
 
